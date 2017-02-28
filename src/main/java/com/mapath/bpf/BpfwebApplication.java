0fba581ea421eb66b4acd5e0199ffbf6ce1cfae6
zhouxiaobo <zhouxiaobo@mapath.com>
2017-02-28 19:25:05 +0800

    add test module

--- src/main/java/com/mapath/bpf/BpfwebApplication.java
+++ src/main/java/com/mapath/bpf/BpfwebApplication.java
@@ -1,10 +1,45 @@
 package com.mapath.bpf;
 
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan({"com.mapath.*"})
+@EnableTransactionManagement
+@EnableScheduling
+@MapperScan({"com.mapath.bpf.mapper"})
 public class BpfwebApplication {
+
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource() {
+		return new com.zaxxer.hikari.HikariDataSource();
+	}
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSource());
+		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+		sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath*:**mapper/*.xml"));
+		return sqlSessionFactoryBean.getObject();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BpfwebApplication.class, args);
--- src/main/java/com/mapath/bpf/controller/NewsController.java
+++ src/main/java/com/mapath/bpf/controller/NewsController.java
@@ -1,12 +1,18 @@
 package com.mapath.bpf.controller;
 
 import com.mapath.bpf.model.NewsModel;
+import com.mapath.bpf.service.AdminService;
 import com.mapath.bpf.service.NewsService;
 import com.mapath.bpf.service.impl.NewsServiceImpl;
 import com.mapath.bpf.utils.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -15,11 +21,28 @@ import java.util.List;
  * Created by zhouxiaobo on 2017/2/28.
  */
 
-@Controller()
+@Controller
 public class NewsController {
+
+     private Logger logger = LoggerFactory.getLogger(NewsController.class);
 
     @Autowired
     private NewsService newsService;
 
+    @RequestMapping(value = "newsList")
+    public String newsList(){
+        logger.info("enter news page");
+        return "news";
+    }
 
+    @RequestMapping(value = "queryNews", method = RequestMethod.POST)
+    public String queryNews(String keyword, Model model){
+        logger.info("execute query");
+
+        List<NewsModel> records = newsService.newslist(keyword);
+        logger.info((records == null)?"0条":records.size() + "条");
+
+        model.addAttribute("news", records);
+        return "news";
+    }
 }
